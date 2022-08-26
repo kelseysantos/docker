34f7f520ab23e7aeca8b5744d9cfd090c9acaaa8
kelseysantos <kelseysantos@yahoo.com.br>
2022-08-26 09:36:33 -0400

    boracuidar

--- AAtestecontainer/start.sh
+++ AAtestecontainer/start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #######################################################################################
 # kelseysantos.blogspot.com - contato:kelseysantos@yahoo.com.br
-# Data Ultima Atualizacao: 20220603
+# Data Ultima Atualizacao: 20220826
 #######################################################################################
+
+ssh-keygen -A
+exec /usr/sbin/sshd -D -e "$@"
 
 while [ true ]; do
     sleep 3600;
